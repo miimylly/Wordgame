3537aa8462b116ca127b531f61f08d316ef0cfbb
Minna <mmartinpaiva@gmail.com>
2019-08-08 15:46:31 +0300

    Muutoksia, paljon muutoksia

---
+++ Minna.py
@@ -0,0 +1,19 @@
+# ensin pitää valmistella
+import sys
+print(sys.path)
+
+#  tämä aloittaa
+print("Minkä sanan haluat etsiä?")
+sana = input()
+index=0
+while index < len(nimi):
+    nimi[index]
+    index +=1
+
+sanalista = open("words.txt", "r", encoding = "uft-8")
+while True:
+    sanat = sanlista.readline()
+    if len(sanalista) == 0:
+        break
+    print(sanalista, end = "")
+sanalista.close()
